1b1bea3155b399b91befd271616d0241007ec34b
MSI\itinose <zhongx457@gmail.com>
2025-01-22 12:04:50 +0900

    新規

--- lib/MainPageWidget.dart
+++ lib/MainPageWidget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/detail/CouponDetail.dart';
 import 'package:flutter_application_1/list/CouponListView.dart';
 
 class MainPageWidget extends StatefulWidget{
